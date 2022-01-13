622e414ddc899bb6e6ecb6d60f97fc280005ef65
matheuscoutsiers <ma.coutsiers@gmail.com>
2022-01-13 15:02:37 +0100

    first commit

---
+++ hello world/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int x; 
+    std::cout << "Type a number: "; // Type a number and press enter
+    std::cin >> x; // Get user input from the keyboard
+    std::cout << "Your number is: " << x; // Display the input value
+
+    return;
+}
